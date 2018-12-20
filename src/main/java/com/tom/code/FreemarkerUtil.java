2afaed33166b44054887a5bb6e09f529745eeca8
tomZhang <310323240@qq.com>
2018-12-20 22:21:14 +0800

    实现增加service

--- src/main/java/com/tom/code/FreemarkerUtil.java
+++ src/main/java/com/tom/code/FreemarkerUtil.java
@@ -14,12 +14,19 @@ public class FreemarkerUtil {
         FileWriter out = null;
         try {
             // 通过一个文件输出流，就可以写到相应的文件中，此处用的是绝对路径
-            out = new FileWriter(new File("C:/workspace/" + "1.txt"));
+            String classString = this.getClass().getResource("").getFile();
+            String fileStrings = classString.replace("target/classes","src/main/java") + "service";
+            File file = new File(fileStrings);
+            if(!file.exists()){
+                file.mkdir();
+            }
+            String fileString = fileStrings+"/1.txt";
+            out = new FileWriter(new File(fileString));
             Map<String,String> map = new HashMap<String, String>();
-            map.put("name","zhang");
+            map.put("name","zhang1");
             // 通过Freemaker的Configuration读取相应的ftl
             Configuration cfg = new Configuration();
-            // 设定去哪里读取相应的ftl模板文件
+            // 设定去哪里读取相应的ftl模板文件 这里是去class的目录下，用getClass().getResource("/").getFile()可获取文件地址
             cfg.setClassForTemplateLoading(this.getClass(), "/ftl");
             // 在模板文件目录中找到名称为name的文件
             Template temp = cfg.getTemplate("2.ftl");
@@ -31,7 +38,8 @@ public class FreemarkerUtil {
 
     public static void main(String[] args) {
         FreemarkerUtil freemarkerUtil = new FreemarkerUtil();
-        System.out.println(freemarkerUtil.getClass().getResource("/").getFile());
+        System.out.println(freemarkerUtil.getClass().getResource("").getFile()+"1.txt");
+//        System.out.println(freemarkerUtil.getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
         freemarkerUtil.generateFile();
     }
 }
